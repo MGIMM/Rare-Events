ef17f2e9e3b93d4e26bf7eb915dca9016908e2bd
MGIMM <duqiming2004@gmail.com>
2016-06-02 12:25:45 +0200

    S_called_times commented// permutation won't work.

---
+++ simulation/estimation_sigma2/pyrare.py
@@ -0,0 +1,242 @@
+import numpy as np
+from time import time
+
+
+class RareEvents:
+    def __init__(self, mu_0, score_function,
+                 shaker,level, p_0 = 0.75):
+        """
+        :param level: level to estimate
+        :param p_0: successful rate
+        :param mu_0: distribution of X_0
+        :param score_function: black box score_function
+        :param shaker: metro-polis/Gibbs kernel
+        """
+        self.mu_0 = mu_0
+        self.score_function = score_function
+        self.shaker = shaker
+        self.level = level
+        self.p_0 = p_0
+
+    def adaptive_levels(self,N, shake_times = 10,\
+                                  reject_rate = 0.5, sigma_default = 0.5,\
+                                descent_step = 0.02, status_tracking = False):
+        ###### Initiation
+        t_0 = time()
+        X = self.mu_0(N)
+        
+        p_0 = self.p_0
+        L = np.array([-np.Inf,np.sort(self.score_function(X))[np.int((1-p_0)*N)]])
+        k = 1
+        #S_called_times = N
+        E = []
+        E += [range(N)]
+        ######
+
+        while(L[k]<self.level):
+            I = []
+            survive_index = []
+            for i in range(N):
+                if self.score_function(X[i])>L[k]:
+                    I += [X[i]]
+                    #S_called_times += 1
+                    survive_index += [i]
+                    
+
+            ell = len(I) 
+       ####### to ensure that I_k would not be empty
+            if ell == 0:        
+                break
+
+       ###### permutation trick to replace multinominal distribution
+            #I = np.random.permutation(I)
+            #q = 0
+            #for i in clone_index:
+            #    X[i] = I[q%ell]
+            #    q += 1
+            A = [] 
+            X_cloned = np.zeros(N)
+            for i in range(N):
+                A += [int(np.random.choice(survive_index))]
+                X_cloned[i] = X[A[i]]
+            ## permutation trick ##
+            #A_permutation = np.random.permutation(survive_index)
+            #for i in range(N):
+            #    A += [A_permutation[i%ell]]
+            #    X_cloned[i] = X[A[i]]
+                
+            X = X_cloned
+            E_iter = []
+            for i in range(N):
+                E_iter += [E[k-1][A[i]]]
+
+            E += [E_iter]
+        ###### shaker
+        ###### we will use an adaptive method to set the parameter of the shaker by
+        ###### controlling the rejection rate.
+
+            
+            for index_shaker in range(shake_times):
+                rate = 1.
+                sigma_1 = sigma_default
+
+                while(rate>reject_rate):
+                    reject = N  
+                    for j in range(N):
+                        X_iter = self.shaker(X[j],sigma_1 = sigma_1)
+                        if self.score_function(X_iter) > L[k]:
+                            #S_called_times += 1
+                            X[j] = X_iter
+                            reject -= 1.
+                    rate = reject/np.float(N)
+                    sigma_1 -= descent_step
+                    if sigma_1 <= 0:
+                        break
+        ######
+            L = np.append(L, np.sort(self.score_function(X))[np.int((1-p_0)*N)])
+            k += 1
+
+        N_L = np.sum((self.score_function(X)>L[-1]))
+        r_hat = N_L/float(N)
+        p_hat = p_0**(k-1)*r_hat 
+        n = k - 1
+        phi = np.zeros(N)
+        for i in range(N):
+            for j in range(N):
+                if E[n][j] == i:
+                    phi[i] += np.float(self.score_function(X[j])>L[-1])
+            phi[i] = phi[i]/float(N)
+
+        phi = np.array(phi)
+        m = (N/float(N-1))**(n+1)*(r_hat**2 - np.sum(phi**2))
+        V = r_hat**2 - m
+        V = V/r_hat**2
+        V *= N
+
+        if status_tracking ==True:
+            print ("estimation of p: " + str(p_hat))
+            print ('____________________________________________________________\n')
+            print ("Time spent: %s s" %(time() - t_0) )
+            #print ("score_function called: %s times" % S_called_times)
+        output = {'p_hat':p_hat,  \
+                 
+                 , 'V': V
+                 }    
+        return output 
+
+##############################fixed levels##############################                
+    def fixed_levels(self,N,L, shake_times = 10,\
+                                  reject_rate = 0.5, sigma_default = 0.5,\
+                                descent_step = 0.02, status_tracking = False):
+        ###### Initiation
+        t_0 = time()
+        X = self.mu_0(N)
+        
+        p_0 = self.p_0
+        k = 1
+        #S_called_times = N
+        list_hat_p =  []
+        E = []
+        E += [range(N)]
+        ######
+
+        while(k <= len(L) -1 ):
+
+            I = []
+            survive_index = []
+            for i in range(N):
+                if self.score_function(X[i])>L[k]:
+                    I += [X[i]]
+                    #S_called_times += 1
+                    survive_index += [i]
+                    
+
+            ell = len(I) 
+
+            if ell == 0:        ###### to ensure that I_k would not be empty
+                break
+            list_hat_p = np.append(list_hat_p, ell/float(N))
+
+       ###### permutation trick to replace multinominal distribution
+            #I = np.random.permutation(I)
+            #q = 0
+            #for i in clone_index:
+            #    X[i] = I[q%ell]
+            #    q += 1
+            ### multinominal cloning ###
+            A = [] 
+            X_cloned = np.zeros(N)
+            for i in range(N):
+                A += [int(np.random.choice(survive_index))]
+                X_cloned[i] = X[A[i]]
+                
+            ## permutation trick ##
+            #A_permutation = np.random.permutation(survive_index)
+            #for i in range(N):
+            #    A += [A_permutation[i%ell]]
+            #    X_cloned[i] = X[A[i]]
+                
+            X = X_cloned
+            E_iter = []
+            for i in range(N):
+                E_iter += [E[k-1][A[i]]]
+
+            E += [E_iter]
+        ###### shaker
+        ###### we will use an adaptive method to set the parameter of the shaker by
+        ###### controlling the rejection rate.
+
+            
+            for index_shaker in range(shake_times):
+                rate = 1.
+                sigma_1 = sigma_default
+
+                while(rate>reject_rate):
+                    reject = N  
+                    for j in range(N):
+                        X_iter = self.shaker(X[j],sigma_1 = sigma_1)
+                        if self.score_function(X_iter) > L[k]:
+                            #S_called_times += 1
+                            X[j] = X_iter
+                            reject -= 1.
+                    rate = reject/np.float(N)
+                    sigma_1 -= descent_step
+                    if sigma_1 <= 0:
+                        break
+        ######
+
+
+            k += 1
+            #X_trace += [X]
+
+        p_hat = np.prod(list_hat_p) 
+        N_L = np.sum((self.score_function(X)>L[-1]))
+        r_hat = N_L/float(N)
+        #p_hat = r_hat*p_hat
+        n = k - 1
+        phi = np.zeros(N)
+        for i in range(N):
+            for j in range(N):
+                if E[n][j] == i:
+                    #phi[i] += np.float(self.score_function(X[j])>L[-1])
+                    phi[i] += 1
+            phi[i] = phi[i]/float(N)
+
+        phi = np.array(phi)
+        #m = (N/float(N-1))**(n+1)*(r_hat**2 - np.sum(phi**2))
+        m = (N/float(N-1))**(n+1)*(1 - np.sum(phi**2))
+        V = 1 - m
+        #V = r_hat**2 - m
+        #V = V/r_hat**2
+        V *= N
+
+        if status_tracking ==True:
+            print ("estimation of p: " + str(p_hat))
+            print ('____________________________________________________________\n')
+            print ("Time spent: %s s" %(time() - t_0) )
+            #print ("score_function called: %s times" % S_called_times)
+        output = {'p_hat':p_hat,  \
+                 
+                 , 'V': V
+                 }    
+        return output 
---
+++ simulation/estimation_sigma2/sigma_hat2.py
@@ -0,0 +1,151 @@
+from pyrare import *
+import sys
+import numpy as np
+from scipy.stats import norm
+from time import time
+from ProgressBar import *
+from matplotlib import pyplot as plt
+from matplotlib.legend_handler import HandlerLine2D
+
+def S_test(X):
+    #score function which is a black box
+    return np.abs(X)
+
+def get_paramS_test(level_test = 8, p_0 = 0.75,status_tracking = True):
+
+    real_p = (1-norm.cdf(level_test))*2
+
+    ###idealized situation
+
+    n_0 = int(np.floor(np.log(real_p)/np.log(p_0)))
+    r = real_p/(p_0**n_0)
+    sigma_theoretical = np.sqrt(n_0*(1-p_0)/p_0 + (1-r)/r)
+    l = [-np.inf]
+    for k in range(1,n_0+1,1):
+        l = np.append(l, norm.ppf(1 - p_0**k/2))
+    l_ideal = np.append(l, level_test)
+
+    if status_tracking == True:
+        print ("p_0 = " + str(p_0) + '\t n_0 =' + str(n_0) + "\t r = " + str(r))
+        #print ("sequence of levels: "+ str(l_ideal))
+        print ("real value of p: " + str(real_p))
+        print ("theoretical relative deviation: " + str(sigma_theoretical))
+    return real_p, sigma_theoretical, n_0, l_ideal
+
+def mu_0_test(n):
+    '''
+    param n: the size of particles
+    '''
+    return np.random.normal(0,1,n)
+
+
+
+#tuning parameter sigma_1 for shaker
+
+
+def shaker_gaussian(x,sigma_1):
+    c = np.sqrt(1+sigma_1**2)
+    return np.random.normal(x/c,sigma_1/c,1)
+
+def shaker_metropolis(x,sigma_1):
+    iter = np.random.uniform(x-sigma_1,x+sigma_1)
+    if np.exp(-0.5*(iter**2-x**2))>np.random.rand(1):
+   	return iter
+    else:
+    	return x
+
+
+print ('\n============================================================')
+####### parameters ######
+p_0_test = 0.7 
+N_test = 1000
+#shaker = shaker_metropolis
+shaker = shaker_gaussian
+shake_times = 1 
+reject_rate = 0.9
+num_simulation = 100
+level_test = 5 
+#def input_parameters():
+#    print ('please input the parameters:\n')
+#    p_0_test = input('p_0_test: ')
+#    N_test = input('number of particles: ')
+#    shake_times = input('shake_times: ')
+#    shaker_choice = 'empty'
+#    while shaker_choice not in ['metropolis', 'gaussian']:
+#        shaker_choice = raw_input("shaker('metropolis' or 'gaussian'): ")
+#    
+#    if shaker_choice == 'metropolis':
+#        shaker = shaker_metropolis
+#    elif shaker_choice == 'gaussian':
+#        shaker = shaker_gaussian
+#    num_simulation = 100
+#    return p_0_test, N_test, shaker, shake_times, num_simulation
+#
+#p_0_test, N_test, shaker, shake_times, num_simulation = input_parameters()
+#########################
+
+test_info = '|num_particles_' + str(N_test) + '|' + \
+        str(shaker).split(' ')[1] + '|shake_times_' + str(shake_times) 
+        
+
+print ('Info: ' + test_info)
+params = get_paramS_test(level_test = level_test, p_0 = p_0_test,status_tracking = True)
+L_ideal = params[3]
+
+rare_test = RareEvents(mu_0 = mu_0_test, score_function = S_test,\
+	level = level_test,shaker = shaker, p_0 = p_0_test)
+
+#test
+#rare_test.adaptive_levels(N = N_test, shake_times = 1,\ 
+# reject_rate = 0.5, sigma_default = 0.5, descent_step = 0.1,status_tracking=True)
+#
+
+##############################
+### simualtion & plotting ####
+##############################
+list_p = []
+list_n_0 = []
+list_r = []
+list_V = []
+list_s_called_times = []
+bar = ProgressBar(total = num_simulation)
+for i in range(num_simulation):
+###ProgressBar###
+    bar.move()
+    bar.log()
+#################
+##fixed_levels
+#    iter_output = rare_test.fixed_levels\
+#            (N = N_test, L = L_ideal,  shake_times = shake_times,reject_rate = reject_rate, \
+#            sigma_default = 0.5, descent_step = 0.1,status_tracking=False)
+
+
+#adaptive_levels
+    iter_output = rare_test.adaptive_levels\
+            (N = N_test, shake_times = shake_times,reject_rate = reject_rate, \
+            sigma_default = 0.5, descent_step = 0.1,status_tracking=False)
+    list_p = np.append(list_p, iter_output['p_hat'])           
+    list_V = np.append(list_V, iter_output['V'])
+#r_hat = np.mean(list_r)
+n_0_real = params[2]
+p_real = params[0]
+#n_0_hat = list_n_0[np.argmax(np.bincount(list_n_0))] 
+#sigma_idealized = np.sqrt(n_0_hat*((1-p_0_test)/p_0_test) + (1-r_hat)/r_hat) 
+p_hat = np.mean(list_p)
+relative_var = (list_p/p_real - 1) * np.sqrt(N_test)
+sigma_empirical = np.var(relative_var)
+#sigma_empirical = np.sum(relative_var**2)/float(num_simulation)
+print ('estimation of p: ' + str(np.mean(list_p)))
+#print ('sigma (theoretical): ' + str(params[1]))
+#print ('sigma (idealized): ' + str(sigma_idealized))
+print ('sigma_tilde2 (empirical): ' + str(sigma_empirical))
+#print ('reject rate: ' + str(reject_rate))
+print ('============================================================\n')
+#print 'relative variation:'
+#print relative_var
+#print 'variance corrected:'
+#print np.var((np.abs(relative_var)<12)*relative_var)
+print 'list_prediction:'
+print list_V
+print 'mean of prediction:'
+print np.mean(list_V)
